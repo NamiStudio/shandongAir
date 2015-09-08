dacd2b68c74cf3e0263eafc1b79a0650c268a733
cxw2014 <841699017@qq.com>
2015-09-08 17:55:18 +0800

    menu achieved by jquery,yep!!! ^_^

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
-  $("ul li").each(
-     $(this).bind("mouseover",function ()
-     {
-       $(this).next("div.menu-hide").css("display","block");
-     });
-     $(this).bind("mouseout",function ()
-     {
-      $(this).next("div.menu-hide").css("display","none");
-     }); 
-  );
+   $("ul li").each(function (){
+      $(this).bind("mouseover",function(){
+         $(this).children("a").css("color","#f98c03");
+         $(this).children('div.menu-hide').css("display","block");
+      });
+      $(this).bind("mouseout",function(){
+        $(this).children("a").css("color","#092d4f");
+        $(this).children('div.menu-hide').css("display","none");
+      })
+   });
 });
