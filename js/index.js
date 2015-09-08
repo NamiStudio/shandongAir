4cd8cddd440768b33a2ac8e0da8d9b279d79da2a
cxw2014 <841699017@qq.com>
2015-09-08 17:15:37 +0800

    i have written menu all day,but failed

--- js/index.js
+++ js/index.js
@@ -1,20 +1,12 @@
 $(document).ready(function(){
-   $("ul li").bind("mouseover",function(){
-   	  $(this).next("div.menu-hide").show();
-   	  $(this).next("div.menu-hide").bind("mouseover",function(){
-   	  	$("ul li div.menu-hide").show();
-   	  });
-   });
-   $("ul li").bind("mouseout",disapear);
-   $("ul li div.menu-hide").bind("mouseout",function(){
-   	   $(this).hide();
-   });
-
-   function disapear(){
-   			$(this).next(".menu-hide").hide();		   	
-   }
-
-
-
-
+  $("ul li").each(
+     $(this).bind("mouseover",function ()
+     {
+       $(this).next("div.menu-hide").css("display","block");
+     });
+     $(this).bind("mouseout",function ()
+     {
+      $(this).next("div.menu-hide").css("display","none");
+     }); 
+  );
 });
