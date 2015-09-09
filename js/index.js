ec4b70d7d9d834ef3c36c157311cb3f725af2e65
cxw2014 <841699017@qq.com>
2015-09-09 11:22:44 +0800

    机票预订1

--- js/index.js
+++ js/index.js
@@ -37,5 +37,57 @@ $(document).ready(function(){
       $("#one").removeClass("active");
       $("#retime").show();
    });
+
+   //单个订单乘机人数不能超过3人！（包括小孩）
+   
+   $(".person-3 .person-add").bind("click",function(){
+      var num1=parseInt($(".person-3 label").text());
+      var num2=parseInt($(".person-0 label").text());
+      if(num1+num2==3){
+         alert("温馨提示:单个订单乘机人数不能超过3人！");
+      }else{
+         num1=num1+1;
+         $(".person-3 label").text(num1);
+      }
+   });
+
+   $(".person-3 .person-minus").bind("click",function(){
+      var num=parseInt($(".person-3 label").text());
+      if(num>1){
+         num--;
+         $(".person-3 label").text(num);
+      }
+   });
+   var flag=false;
+   $(".person-0 .person-add").bind("click",function(){
+      var num1=parseInt($(".person-3 label").text());
+      var num2=parseInt($(".person-0 label").text());
+      if(flag==false){
+         flag=true;
+         confirm("温馨提示：携带儿童的成人需年满18周岁且是具有民事行为能力的成人，如果是请点击确认，否则点击取消！");
+      }
+      if(num1+num2==3){
+         alert("温馨提示:单个订单乘机人数不能超过3人！");
+      }else{
+         num2=num2+1;
+         $(".person-0 label").text(num2);
+      }
+   });
+   $(".person-0 .person-minus").bind("click",function(){
+      var num=parseInt($(".person-0 label").text());
+      if(num>0){
+         num--;
+         $(".person-0 label").text(num);
+      }
+   });
+   function addrmust(){
+      var text1=$("#departure").text();
+      var text2=$("#destination").text();
+      if(text1==""||text2==""){
+         alert("温馨提示：请完整填写出发地和目的地！");
+      }
+   }
+   $("#gotime").bind("click",addrmust);
+   $("#retime").bind("click",addrmust);
    
 });
